b08a21fa76ee25c879e35620e17216a9246fcd25
Eliane V <eliane.vdc@protonmail.com>
2022-11-29 12:35:44 +0000

    render icons socials

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header className="p-4 h-14 w-full fixed z-40 backdrop-blur-lg shadow-sm">
+    <div className="max-w-7xl p-4 h-14 w-full fixed z-40 backdrop-blur-lg">
       <div className="flex justify-between">
         <div className="pl-6 font-bold before:absolute before:bg-yellow-500 before:w-6 before:h-6 before:left-2 before:rounded-full">
           ELIANE
@@ -11,6 +11,6 @@ export default function Navbar() {
           <a className="ml-6">Contact</a>
         </nav>
       </div>
-    </header>
+    </div>
   );
 }
