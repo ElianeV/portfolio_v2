5c754cd7ee8d6d2f3bd4a25ab33c5de4a627d35a
Eliane V <eliane.vdc@protonmail.com>
2022-12-20 12:54:27 +0000

    test form 1

--- components/contactForm.js
+++ components/contactForm.js
@@ -2,20 +2,22 @@ import Link from "next/link";
 
 export default function ContactForm() {
   return (
-    <form
-      name="contact-form"
-      method="POST"
-      action="contact/?success=true"
-      data-netlify="true"
-    >
-      <input type="hidden" name="form-name" value="contact-form" />
-      <label htmlFor="name">Name *</label>
-      <input id="name" name="name" required type="text" />
-      <label htmlFor="email">E-mail Address *</label>
-      <input id="email" type="email" name="email" required />
-      <label htmlFor="message">Message *</label>
-      <textarea id="message" name="message" required></textarea>
-      <button type="submit">Submit</button>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" />
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email" />
+      </p>
+      <p>
+        <label for="message">Message</label>
+        <textarea id="message" name="message"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
     </form>
   );
 }
